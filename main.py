a7af432f77acbd18fd5da677e8667e03e8bf9775
cortanaRAT <husseincc43@gmail.com>
2025-09-10 19:31:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ BINANCE_API_KEY = "f9cdfdd0f2b13fb8bb89ef5b9edf93281b2fef3aa3e8ff16d48817b4f59c3
 BINANCE_API_SECRET = "f7b69a165a2ba1ea72727cf96c908863eafa1bff3673dd6752cc193e20734f70"
 
 client = Client(BINANCE_API_KEY, BINANCE_API_SECRET, testnet=True)
-
+client.API_URL = "https://testnet.binance.vision/api"
 # ========== Flask ==========
 app = Flask(__name__)
 
