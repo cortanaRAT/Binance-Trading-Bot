2e3486cee917581b434153c582172ab4044018ee
cortanaRAT <husseincc43@gmail.com>
2025-09-10 17:51:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import os
 app = Flask(__name__)
 
 # استدعاء الـ API Key و Secret من Environment Variables
-API_KEY = os.getenv("BINANCE_API_KEY")
-API_SECRET = os.getenv("BINANCE_API_SECRET")
+API_KEY = "f9cdfdd0f2b13fb8bb89ef5b9edf93281b2fef3aa3e8ff16d48817b4f59c3543"
+")
+API_SECRET = "f7b69a165a2ba1ea72727cf96c908863eafa1bff3673dd6752cc193e20734f70"
 
 # Client مع testnet=True
 client = Client(API_KEY, API_SECRET, testnet=True)
